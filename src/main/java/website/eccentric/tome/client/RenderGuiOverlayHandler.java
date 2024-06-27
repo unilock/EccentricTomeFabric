a781afbcf8be9ba0f3a08ba499040167c293ecbe
unilock <unilock@fennet.rentals>
2024-06-27 10:32:21 -0400

    Fix indentation (again)
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/website/eccentric/tome/client/RenderGuiOverlayHandler.java
+++ src/main/java/website/eccentric/tome/client/RenderGuiOverlayHandler.java
@@ -12,57 +12,57 @@ import website.eccentric.tome.Tome;
 import website.eccentric.tome.TomeItem;
 
 public class RenderGuiOverlayHandler {
-    public static void onRender(GuiGraphics gui, float tickDelta) {
-        var minecraft = Minecraft.getInstance();
+	public static void onRender(GuiGraphics gui, float tickDelta) {
+		var minecraft = Minecraft.getInstance();
 
-        var player = minecraft.player;
-        if (player == null)
-            return;
+		var player = minecraft.player;
+		if (player == null)
+			return;
 
-        var level = minecraft.level;
-        if (level == null)
-            return;
+		var level = minecraft.level;
+		if (level == null)
+			return;
 
-        var hit = minecraft.hitResult;
-        if (!(hit instanceof BlockHitResult))
-            return;
+		var hit = minecraft.hitResult;
+		if (!(hit instanceof BlockHitResult))
+			return;
 
-        var blockHit = (BlockHitResult) hit;
+		var blockHit = (BlockHitResult) hit;
 
-        var hand = Tome.inHand(minecraft.player);
-        if (hand == null)
-            return;
+		var hand = Tome.inHand(minecraft.player);
+		if (hand == null)
+			return;
 
-        var state = level.getBlockState(blockHit.getBlockPos());
-        if (state.isAir())
-            return;
+		var state = level.getBlockState(blockHit.getBlockPos());
+		if (state.isAir())
+			return;
 
-        var tome = player.getItemInHand(hand);
-        if (!(tome.getItem() instanceof TomeItem))
-            return;
+		var tome = player.getItemInHand(hand);
+		if (!(tome.getItem() instanceof TomeItem))
+			return;
 
-        var mod = ModName.from(state);
-        var modsBooks = Tome.getModsBooks(tome);
-        if (!modsBooks.containsKey(mod))
-            return;
+		var mod = ModName.from(state);
+		var modsBooks = Tome.getModsBooks(tome);
+		if (!modsBooks.containsKey(mod))
+			return;
 
-        var books = modsBooks.get(mod);
-        if (books.isEmpty())
-            return;
+		var books = modsBooks.get(mod);
+		if (books.isEmpty())
+			return;
 
-        var book = books.get(books.size() - 1);
-        var hoverName = book.getHoverName();
-        var convert = I18n.get("eccentrictome.convert");
+		var book = books.get(books.size() - 1);
+		var hoverName = book.getHoverName();
+		var convert = I18n.get("eccentrictome.convert");
 
-        RenderSystem.enableBlend();
-        RenderSystem.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		RenderSystem.enableBlend();
+		RenderSystem.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 
-        var window = Minecraft.getInstance().getWindow();
-        var x = window.getGuiScaledWidth() / 2 - 17;
-        var y = window.getGuiScaledHeight() / 2 + 2;
+		var window = Minecraft.getInstance().getWindow();
+		var x = window.getGuiScaledWidth() / 2 - 17;
+		var y = window.getGuiScaledHeight() / 2 + 2;
 
-        gui.renderItem(book, x, y);
-        gui.drawString(minecraft.font, hoverName, x + 20, y + 4, 0xFFFFFFFF);
-        gui.drawString(minecraft.font, ChatFormatting.GRAY + convert, x + 25, y + 14, 0xFFFFFFFF);
-    }
+		gui.renderItem(book, x, y);
+		gui.drawString(minecraft.font, hoverName, x + 20, y + 4, 0xFFFFFFFF);
+		gui.drawString(minecraft.font, ChatFormatting.GRAY + convert, x + 25, y + 14, 0xFFFFFFFF);
+	}
 }
